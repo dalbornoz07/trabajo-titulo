648fc897dfa6f96129bdc78f5f241df7c1835e17
dalbornoz07 <diego.albornoz07@gmail.com>
2022-11-18 18:20:14 -0300

    Estructura coockicutter

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# Datos Trabajo de título UTFSM
+Trabajo de título: Caracterización de los estudiantes de programación según su desempeño: un modelo predictivo 
+==============================
 
-En este repositorio, se encuentran algunos de los archivos generados durante el desarrollo del trabajo de título del author, los cuales fueron construidos a partir de las calificaciones de los estudiantes del ramo de Programación de la Universidad Técnica Federico Santa María.
+Trabajo de título realizado por  el estudiante Diego Albornoz Ley para optar al título de Ingniero Civil Informático en la Universidad Técnica Federico Santa María.
+
+
+Hoy en día, para las carreras y universidades un alto índice de reprobación puede significar en la reducción de años de acreditación que posee, afectar el ambiente universitario, como también generar un desfase respecto a la malla curricular por parte del estudiante. Es por ello que la asignatura de programación IWI-131 de la Universidad Técnica Federico Santa María requiere contar con herramientas o mecanismos que permitan detectar tempranamente aquellos estudiantes con mayor probabilidad de reprobar, para ello se utilizó un enfoque de minería de datos para obtener una caracterización del comportamiento de los estudiantes y
+la influencia que tiene en el rendimiento de la asignatura, generando agrupamientos, reglas de asociación, entre otros; logrando así identificar grupos de comportamientos y tendencias que permitirán tomar decisiones en base a ellos.
+
+Keywords: Caracterización, rendimiento académico estudiantes, Minería de datos Educacional
+
+
+Organización de proyecto
+------------
+
+    ├── LICENSE
+    ├── Makefile           <- Makefile with commands like `make data` or `make train`
+    ├── README.md          <- The top-level README for developers using this project.
+    ├── data
+    │   ├── external       <- Data from third party sources.
+    │   ├── interim        <- Intermediate data that has been transformed.
+    │   ├── processed      <- The final, canonical data sets for modeling.
+    │   └── raw            <- The original, immutable data dump.
+    │
+    ├── docs               <- A default Sphinx project; see sphinx-doc.org for details
+    │
+    ├── models             <- Trained and serialized models, model predictions, or model summaries
+    │
+    ├── notebooks          <- Jupyter notebooks. Naming convention is a number (for ordering),
+    │                         the creator's initials, and a short `-` delimited description, e.g.
+    │                         `1.0-jqp-initial-data-exploration`.
+    │
+    ├── references         <- Data dictionaries, manuals, and all other explanatory materials.
+    │
+    ├── reports            <- Generated analysis as HTML, PDF, LaTeX, etc.
+    │   └── figures        <- Generated graphics and figures to be used in reporting
+    │
+    ├── requirements.txt   <- The requirements file for reproducing the analysis environment, e.g.
+    │                         generated with `pip freeze > requirements.txt`
+    │
+    ├── setup.py           <- makes project pip installable (pip install -e .) so src can be imported
+    └── src                <- Source code for use in this project.
+       ├── __init__.py    <- Makes src a Python module
+       │
+       ├── data           <- Scripts to download or generate data
+       │   └── make_dataset.py
+       │
+       ├── features       <- Scripts to turn raw data into features for modeling
+       │   └── build_features.py
+       │
+       ├── models         <- Scripts to train models and then use trained models to make
+       │   │                 predictions
+       │   ├── predict_model.py
+       │   └── train_model.py
+       │
+       └── visualization  <- Scripts to create exploratory and results oriented visualizations
+           └── visualize.py
+
+
---
+++ src/data/make_dataset.py
@@ -0,0 +1,165 @@
+"""
+Author: Diego Albornoz
+Versión: 1.5
+Updated: 08-11-2022
+Created: 01-11-2022
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import sys
+from typing import List
+from multiprocessing import Process, Lock
+
+#from selenium import webdriver
+#from selenium.webdriver.common.by import by
+
+#driver = webdriver.Firefox()
+
+#Global variables
+HOME = '/'.join(os.getcwd().split('/')[:-2])
+RAW= os.path.join(HOME,'data/raw')
+PROCESSED = os.path.join(HOME,'data/processed')
+INTERIM = os.path.join(HOME,'data/interim')
+
+CAMPUS = {0:"CC",1:"CSSJ",2:"Vitacura"}
+
+def get_files_list(condition:str='')->List[str]:
+    """
+    Descripción: Obtiene el listado de archivos dentro de la carpeta data/raw
+    Parametros: condition (str) formato que deben tener los archivos a BUSCAR
+    Retorno: Listado con los nombres de cada archivo que se encuentra en el directorio data/raw
+    """
+    return [ os.path.join(root,name) for root,_,filename in os.walk(os.path.join(RAW,condition),topdown=True)  for name in filename ]
+
+
+def gen_list_cols(text:str,start:int=1,end:int=12)->List[str]:
+    """
+    Descripción:
+    Parámetros:
+    Retorno:
+    """
+    return [text.format(i) for i in range(start,end)]
+
+
+def create_campus_csv(archivo:str,anno:int,semestre:int)->None:
+    """
+    Descripción: 
+    Parámetros: - archivo(str) nombre del archivo en excel que se va trabajar
+                - anno (int) año trabajado
+                - semestre (int) semestre trabajado (1 o 2)
+    Retorno: - 
+    """
+    df_template= pd.DataFrame({"RUT":[],"DV.1":[],"Nombre":[],"VTR":[],"C1":[],"C2":[],"CR":[],
+                            'S1':[],'S2':[],'S3':[],'S4':[],'S5':[],'S6':[],'S7':[],'S8':[],'S9':[],'S10':[],
+                            'F1':[],'F2':[],'F3':[],'F4':[],'F5':[],'F6':[],'F7':[],'F8':[],'F9':[],'F10':[],
+                            'T1':[],'T2':[],'T3':[],'T4':[],'T5':[],'T6':[],'T7':[],'T8':[],'T9':[],'T10':[],
+                            'SM1':[],'SM2':[],'SM3':[],'SM4':[],'SM5':[],'SM6':[],'SM7':[],'SM8':[],"NF":[],
+                            "Carrera_origen":[],"Carrera":[],'Campus':[],"Sexo":[],'Sem':[],'Anno':[]})
+    df_template['Carrera'] = pd.Series(df_template['Carrera'], dtype='string')
+
+    id_campus = [key for key,val in CAMPUS.items() if val in archivo][0]
+    
+    assert archivo.endswith('.xlsx'), "Error en extensión de archivo, solo se acepta xlsx"
+    df_base = pd.read_excel(pd.ExcelFile(archivo),None,na_filter=False,skiprows=3,header=0)
+    for sheet in list(df_base.keys()):
+        if sheet in ['Estadísticas','SUMATORIAS','Cuestionario']:
+            continue
+      
+        sheet_data = df_base[sheet]
+        sheet_data.replace(['','-','#¡DIV/0!'],np.nan,inplace=True)
+        sheet_data.dropna(subset=['RUT'],inplace=True,axis=0)
+
+        _columns = ['RUT','DV.1','Nombre','VTR','C1','C2','CR','NF','Carrera']
+
+        controles = gen_list_cols('S{}',1,11)
+        tareas = gen_list_cols('T{}',1,11)
+        formativos = gen_list_cols('F{}',1,11)
+        actividades = gen_list_cols('SM{}',1,9)
+    
+        sheet_data.fillna(0,inplace=True)
+        
+        sheet_data[controles] = sheet_data[controles].apply(lambda x: x.astype(str).str.replace(',','.')).astype(float)
+        sheet_data[tareas] = sheet_data[gen_list_cols('S{}.1',1,11)].apply(lambda x: x.astype(str).str.replace(',','.')).astype(float)
+        sheet_data[formativos] = sheet_data[gen_list_cols('S{}.2',1,11)].apply(lambda x: x.astype(str).str.replace(',','.')).astype(float)
+        sheet_data[actividades] = sheet_data[gen_list_cols('S{}.3',2,10)].apply(lambda x: x.astype(str).str.replace(',','.')).astype(float)
+        sheet_data['Carrera_origen']= sheet_data.Rol.str[4:7]
+
+        df_template = pd.concat(
+            [df_template,
+            sheet_data[_columns+controles+tareas+formativos+actividades+['Carrera_origen']]
+            ])
+    
+
+    df_template.Sem=semestre
+    df_template.Anno=anno
+    df_template.Campus = id_campus
+    df_template.fillna(0,inplace=True)
+
+    df_template.to_csv(
+        f'{INTERIM}/{anno}-{semestre}/{anno}-{semestre}_{CAMPUS[id_campus]}.csv',
+        decimal='.',
+        index=False)
+
+
+
+def create_campus_csv_AULA(archivo:str,anno:int,semestre:int)->None:
+    id_campus = [key for key,val in CAMPUS.items() if val in archivo][0]
+    df_base = pd.read_excel(pd.ExcelFile(archivo),None,na_filter=False,skiprows=0,header=0)
+    
+    for sheet in list(df_base.keys()):
+        sheet_data = df_base[sheet]
+      
+        df=pd.DataFrame()
+
+        df[['RUT','DV.1']] = tuple(sheet_data['Número de ID'].str.split('-'))
+        df['Nombre']= sheet_data['Nombre']
+        df[['C1','C2','CR']] = sheet_data.filter(regex='Total Certamen').iloc[:,0:]
+        df[[f'S{i}' for i in range(1,11)]] = sheet_data.filter(regex='Cuestionario:Control').iloc[:,0:10]
+        df[[f'F{i}' for i in range(1,11)]] = sheet_data.filter(regex='Total UVA').iloc[:,0:10]
+        df[[f'T{i}' for i in range(1,11)]] = sheet_data.filter(regex='Tarea:Tarea').iloc[:,0:10]
+        df[[f'SM{i}' for i in range(1,9)]] = np.nan
+        df['Carrera'] = sheet_data['Departamento']
+        df['Campus'] = id_campus
+        df['Sem']= semestre
+        df['Anno']= anno
+
+        df.replace(['-'],0.0,inplace=True)
+        df.to_csv(
+        f'{INTERIM}/{anno}-{semestre}/{anno}-{semestre}_{CAMPUS[id_campus]}_AULA.csv',
+        decimal='.',
+        index=False)
+
+
+
+def main():
+    threads=[]
+    temp='{}-{}'
+    
+    annos = ['2020-2','2021-1','2021-2']
+    
+    for i in annos:
+        print(i)
+        year,sem = i.split('-')
+        year = int(year)
+        sem = int(sem)
+        file_list=get_files_list(temp.format(year,sem))
+        
+        if not os.path.isdir(f'{INTERIM}/{year}-{sem}'):
+            os.mkdir(f'{INTERIM}/{year}-{sem}')
+
+        for f in file_list:
+            func = create_campus_csv
+            if 'AULA.xlsx' in f.split('_'):
+                func= create_campus_csv_AULA
+
+            t = Process(target=func,args=[f,year,sem],daemon=True)
+            threads.append(t)
+            t.start()
+            
+    for t in threads:
+        t.join()
+
+if __name__=='__main__':
+    main()
